8b58a26ad2f2beb9a138a4a42b3f5ccf40fab029
FR_Jess <fr_jess@icloud.com>
2022-11-11 14:28:33 +0100

    [add] api

--- js/script.js
+++ js/script.js
@@ -3,14 +3,50 @@ const {createApp} = Vue;
 createApp({
 
   data(){
-
+    return{
+      title: 'Ajax Game',
+      giocatoreA: 0,
+      giocatoreB: 0,
+      isLoaded: false,
+    }
   },
 
   methods:{
+    getApi(){
+      axios.get('https://flynn.boolean.careers/exercises/api/random/int')
+      .then( risultato => {
+        this.giocatoreA = risultato.data.response
+      })
+      .catch( errore => {
+        this.title = errore.code
+      })
+      
+      axios.get('https://flynn.boolean.careers/exercises/api/random/int')
+      .then( risultato => {
+        this.giocatoreB = risultato.data.response
+        this.isLoaded = true;
+      })
+      .catch( errore => {
+        this.title = errore.code
+      })
+    },
+
+    getResult(){
+      let result;
+      if(this.giocatoreA > this.giocatoreB){
+        result = 'Vince A';
+      }else if(this.giocatoreA < this.giocatoreB){
+        result = 'Vince B';
+      }else{
+        result = 'PARI';
+      }
+      return result;
+    }
 
   },
 
   mounted(){
+    this.getApi();
 
   }
 }).mount('#app')
